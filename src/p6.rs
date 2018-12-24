1ee357f4e797e006e2a36067b11b2a50206fefc5
Hourann <tdkgn@outlook.com>
2018-12-24 14:52:07 +0800

    optimize p6

--- src/p6.rs
+++ src/p6.rs
@@ -3,32 +3,21 @@ pub struct Solution;
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        if num_rows < 2 {
-            return s;
-        }
+        if num_rows < 2 { return s; }
         let sb = s.as_bytes();
+        let len = sb.len();
         let num_rows = num_rows as usize;
         let chunk_size = num_rows * 2 - 2;
-        let mut rows = Vec::with_capacity(num_rows);
+        let mut buf = String::with_capacity(len);
         for i in 0..num_rows {
-            let row:Vec<u8> = if i == 0 || i == num_rows - 1 {
-                sb.iter().skip(i).step_by(chunk_size).cloned().collect()
-            } else {
-                let mut row = Vec::with_capacity(2 * sb.len() / chunk_size);
-                let mut offset = 0;
-                while offset + i < sb.len() {
-                    row.push(sb[offset + i]);
-                    offset += chunk_size;
-                    if offset - i < sb.len() {
-                        row.push(sb[offset - i]);
-                    }
+            for j in (0..).step_by(chunk_size).take_while(|&j| i + j < len) {
+                buf.push(sb[i + j] as char);
+                if i != 0 && i != num_rows - 1 && j + chunk_size - i < len {
+                    buf.push(sb[j + chunk_size - i] as char);
                 }
-                row
-            };
-            rows.push(row);
+            }
         }
-        let bytes: Vec<_> = rows.into_iter().flatten().collect();
-        String::from_utf8(bytes).unwrap()
+        buf
     }
 }
 
@@ -50,5 +39,6 @@ mod tests {
     parameterized! {
         three_lines: ("LEETCODEISHIRING", 3, "LCIRETOESIIGEDHN"),
         four_lines: ("LEETCODEISHIRING", 4, "LDREOEIIECIHNTSG"),
+        single_char: ("A", 3, "A"),
     }
 }
