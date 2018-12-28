c2d0aa27b25b680f6aebaa365db924a03c351201
Hourann <tdkgn@outlook.com>
2018-12-28 17:27:00 +0800

    p26

---
+++ src/p26.rs
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array
+pub struct Solution;
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        if nums.is_empty() { return 0; }
+        let mut i = 0;
+        for j in 0..nums.len() {
+            if nums[i] < nums[j] {
+                i += 1;
+                nums[i] = nums[j];
+            }
+        }
+        i as i32 + 1
+
+        // or use built-in method:
+        // nums.dedup(); nums.len() as i32;
+    }
+}
