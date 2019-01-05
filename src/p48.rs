ea7b93063f270be63e81b812391722d6e75a506f
Hourann <tdkgn@outlook.com>
2019-01-05 13:37:40 +0800

    p48

---
+++ src/p48.rs
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/rotate-image/
+
+pub struct Solution;
+
+impl Solution {
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        let len = matrix.len();
+        for i in 0..(len/2) {
+            for j in i..(len - i - 1) {
+                let tmp = matrix[i][j];
+                matrix[i][j] = matrix[len-j-1][i];
+                matrix[len-j-1][i] = matrix[len-i-1][len-j-1];
+                matrix[len-i-1][len-j-1] = matrix[j][len-i-1];
+                matrix[j][len-i-1] = tmp;
+            }
+        }
+    }
+}
